685dc040060ae6c9c46e9a561aece74baa8c4810
MaksymMykhailets <149175463+MaksymMykhailets@users.noreply.github.com>
2025-01-12 17:36:00 +0100

    Enhance Pagination component

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,5 +1,9 @@
 import PropTypes from "prop-types";
 import css from "./Pagination.module.css";
+import { FaAngleLeft } from "react-icons/fa6";
+import { FaAnglesLeft } from "react-icons/fa6";
+import { FaAngleRight } from "react-icons/fa6";
+import { FaAnglesRight } from "react-icons/fa6";
 
 const Pagination = ({ currentPage, totalPages, onPageChange }) => {
   const handleFirstPage = () => onPageChange(1);
@@ -7,27 +11,65 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
   const handlePreviousPage = () => onPageChange(currentPage - 1);
   const handleNextPage = () => onPageChange(currentPage + 1);
 
-  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+  const renderPages = () => {
+    const pages = new Set();
+  
+    if (totalPages > 5) {
+      if (currentPage <= 3) {
+        for (let i = 1; i <= 3; i++) {
+          pages.add(i);
+        }
+        pages.add("...");
+      } else if (currentPage > 3 && currentPage < totalPages - 2) {
+        pages.add(1);
+        pages.add("...");
+        pages.add(currentPage - 1);
+        pages.add(currentPage);
+        pages.add(currentPage + 1);
+        pages.add("...");
+      } else {
+        pages.add(1);
+        pages.add("...");
+        for (let i = totalPages - 2; i <= totalPages; i++) {
+          pages.add(i);
+        }
+      }
+    } else {
+      for (let i = 1; i <= totalPages; i++) {
+        pages.add(i);
+      }
+    }
+  
+    return Array.from(pages);
+  };
 
   return (
     <div className={css.pagination}>
+    <div className={css.flipWrapper}>
       <button
         onClick={handleFirstPage}
         disabled={currentPage === 1}
-        className={css.button}
+        className={css.buttonFlip}
       >
-        «
+        <FaAnglesLeft/>
       </button>
       <button
         onClick={handlePreviousPage}
         disabled={currentPage === 1}
-        className={css.button}
+        className={css.buttonFlip}
       >
-        ‹
+        <FaAngleLeft/>
       </button>
-      {pages.map((page) => (
+    </div>
+    <div className={css.pagesWrapper}>
+    {renderPages().map((page, index) =>
+      page === "..." ? (
+        <span key={`ellipsis-${index}`} className={css.ellipsis}>
+          ...
+        </span>
+      ) : (
         <button
-          key={page}
+          key={`page-${page}`}
           onClick={() => onPageChange(page)}
           className={`${css.button} ${
             page === currentPage ? css.active : ""
@@ -35,21 +77,25 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
         >
           {page}
         </button>
-      ))}
+      )
+    )}
+    </div>
+      <div className={css.flipWrapper}>
       <button
         onClick={handleNextPage}
         disabled={currentPage === totalPages}
-        className={css.button}
+        className={css.buttonFlip}
       >
-        ›
+        <FaAngleRight />
       </button>
       <button
         onClick={handleLastPage}
         disabled={currentPage === totalPages}
-        className={css.button}
+        className={css.buttonFlip}
       >
-        »
+        <FaAnglesRight />
       </button>
+      </div>
     </div>
   );
 };
